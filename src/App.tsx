7b1a5387369ed939b41a6b75477fdb5575790e97
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-15 16:06:28 +0200

    ref(styles): add font sizes

--- src/App.tsx
+++ src/App.tsx
@@ -8,117 +8,8 @@ import { open } from "@tauri-apps/api/dialog";
 import { desktopDir } from "@tauri-apps/api/path";
 import { Store } from "tauri-plugin-store-api";
 import { toast } from "react-hot-toast";
+import { FONTS } from "./fonts";
 const store = new Store(".settings.dat");
-
-import { CSSProperties } from "react";
-
-interface Font {
-  name: string;
-  styles: CSSProperties[];
-}
-
-const FONTS: Font[] = [
-  {
-    name: "Mockary Personal",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Comic Sans MS",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Golden Hills DEMO",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Hello Valentica",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Mailine Personal User",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Sindentosa",
-    styles: [{ fontWeight: 700 }, { fontWeight: 400, fontStyle: "italic" }],
-  },
-  {
-    name: "French Script MT",
-    styles: [{ fontWeight: 700 }],
-  },
-  {
-    name: "Beauty Queen Script",
-    styles: [{ fontWeight: 700 }],
-  },
-  {
-    name: "Cac Lasko Even Weight",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Brush Script MT",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Charlie",
-    styles: [{ fontWeight: 700 }],
-  },
-  {
-    name: "Janda Swirly Twirly",
-    styles: [{ fontWeight: 700 }],
-  },
-  {
-    name: "Ink Free",
-    styles: [{ fontWeight: 700 }],
-  },
-  {
-    name: "Harrigton",
-    styles: [{ fontWeight: 700 }],
-  },
-  {
-    name: "Civitype FG",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "BillyanaPersonalUseOnly",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Marguaritas",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Cursive Standard",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Qwerty Ability-PersonalUse",
-    styles: [{ fontWeight: 700 }],
-  },
-  {
-    name: "White Dream PERSO",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Femina",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Lovely Valentine",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "From Me 2 You",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "Autography",
-    styles: [{ fontWeight: 400 }],
-  },
-  {
-    name: "PrettyGirlsScriptDemo",
-    styles: [{ fontWeight: 700 }],
-  },
-];
 
 function App() {
   const [text, setText] = useState("");
@@ -145,9 +36,7 @@ function App() {
           saveFolder: folder,
           text,
         });
-        result
-          ? toast.success("Image générée !", { id: toastId })
-          : toast.error("Erreur !", { id: toastId });
+        result ? toast.success("Image générée !", { id: toastId }) : toast.error("Erreur !", { id: toastId });
       })
       .catch((err) => {
         console.error(err);
@@ -181,46 +70,23 @@ function App() {
   return (
     <div className="flex flex-col gap-4 p-4">
       <div className="flex flex-col gap-2">
-        <Input
-          className="w-full grow"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          label="Texte"
-        />
+        <Input className="w-full grow" value={text} onChange={(e) => setText(e.target.value)} label="Texte" />
         <div className="inline-flex items-center w-full gap-2">
-          <Button
-            color="default"
-            className="px-8 grow"
-            variant="bordered"
-            onClick={selectFolder}
-          >
+          <Button color="default" className="px-8 grow" variant="bordered" onClick={selectFolder}>
             <MdFolderOpen className="w-5 h-5 shrink-0" />
             Changer le dossier
           </Button>
-          <Button
-            onClick={htmlToImageConvert}
-            color="primary"
-            className="px-8 grow"
-            variant="flat"
-          >
+          <Button onClick={htmlToImageConvert} color="primary" className="px-8 grow" variant="flat">
             <MdCheck className="w-5 h-5 shrink-0" />
             Générer
           </Button>
         </div>
-        <p className="text-sm text-gray-500">
-          Dossier de sauvegarde : {saveFolder}
-        </p>
+        <p className="text-sm text-gray-500">Dossier de sauvegarde : {saveFolder}</p>
       </div>
       <hr />
-      <div
-        className="flex flex-col items-start p-2 -my-4 gap-y-1"
-        ref={elementRef}
-      >
+      <div className="flex flex-col items-start p-4 -my-4 gap-y-1" ref={elementRef}>
         {FONTS.map((font, i) => (
-          <div
-            className="inline-flex items-center w-full gap-2"
-            key={font.name}
-          >
+          <div className="inline-flex items-center w-full gap-2" key={font.name}>
             <p className="w-3 text-xs">{i + 1}</p>
             {font.styles.map((style) => (
               <p
---
+++ src/fonts.ts
@@ -0,0 +1,109 @@
+import { CSSProperties } from "react";
+
+interface Font {
+  name: string;
+  styles: CSSProperties[];
+}
+
+export const FONTS: Font[] = [
+  {
+    name: "Mockary PERSONAL USE ONLY",
+    styles: [{ fontWeight: 400, fontSize: 20 }],
+  },
+  {
+    name: "Comic Sans MS",
+    styles: [{ fontWeight: 400 }],
+  },
+  {
+    name: "Golden Hills DEMO",
+    styles: [{ fontWeight: 400, fontSize: 18 }],
+  },
+  {
+    name: "Hello Valentica",
+    styles: [{ fontWeight: 400, fontSize: 18 }],
+  },
+  {
+    name: "Marline Personal Use",
+    styles: [{ fontWeight: 400, fontSize: 24 }],
+  },
+  {
+    name: "Sindentosa",
+    styles: [{ fontWeight: 400, fontStyle: "italic", fontSize: 18 }],
+  },
+  {
+    name: "French Script MT",
+    styles: [{ fontWeight: 700, fontSize: 20 }],
+  },
+  {
+    name: "Beauty Queen Script",
+    styles: [{ fontWeight: 700, fontSize: 16 }],
+  },
+  {
+    name: "CAC Lasko Even Weight",
+    styles: [{ fontWeight: 400, fontSize: 19 }],
+  },
+  {
+    name: "Brush Script MT",
+    styles: [{ fontWeight: 400, fontSize: 18 }],
+  },
+  {
+    name: "Charlie",
+    styles: [{ fontWeight: 700, fontSize: 18 }],
+  },
+  {
+    name: "Janda Swirly Twirly",
+    styles: [{ fontWeight: 700, fontSize: 18 }],
+  },
+  {
+    name: "Ink Free",
+    styles: [{ fontWeight: 700, fontSize: 14 }],
+  },
+  {
+    name: "Harrington",
+    styles: [{ fontWeight: 700, fontSize: 14 }],
+  },
+  {
+    name: "Civitype FG",
+    styles: [{ fontWeight: 400, fontSize: 26 }],
+  },
+  {
+    name: "BillyanaPersonalUseOnly",
+    styles: [{ fontWeight: 400, fontSize: 18 }],
+  },
+  {
+    name: "Marguaritas",
+    styles: [{ fontWeight: 400, fontSize: 16 }],
+  },
+  {
+    name: "Cursive standard",
+    styles: [{ fontWeight: 400, fontSize: 28 }],
+  },
+  {
+    name: "Qwerty Ability - Personal Use",
+    styles: [{ fontWeight: 700, fontSize: 18 }],
+  },
+  {
+    name: "White Dream PERSONAL USE ONLY",
+    styles: [{ fontWeight: 400, fontSize: 16 }],
+  },
+  {
+    name: "Femina",
+    styles: [{ fontWeight: 400, fontSize: 28 }],
+  },
+  {
+    name: "Lovely Valentine",
+    styles: [{ fontWeight: 400, fontSize: 24 }],
+  },
+  {
+    name: "From Me 2 You",
+    styles: [{ fontWeight: 400, fontSize: 11 }],
+  },
+  {
+    name: "Autography",
+    styles: [{ fontWeight: 400, fontSize: 18 }],
+  },
+  {
+    name: "Pretty Girls Script Demo",
+    styles: [{ fontWeight: 700, fontSize: 16 }],
+  },
+];
